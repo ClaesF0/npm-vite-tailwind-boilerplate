d2d3f17a44ea132c21db34dd9f8b377e75d71e2d
ClaesF0 <claes.folkestad@stud.noroff.no>
2022-10-25 13:59:19 +0200

    prettier test

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
+import "./style.css";
+import javascriptLogo from "./javascript.svg";
 
-document.querySelector('#app').innerHTML = `
+document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
@@ -17,6 +17,6 @@ document.querySelector('#app').innerHTML = `
       Click on the Vite logo to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector("#counter"));
